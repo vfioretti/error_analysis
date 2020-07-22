0f32c3d56c5742eeef5e8bc086a34db1b21e78ea
Valentina <fioretti@iasfbo.inaf.it>
2020-07-23 01:07:05 +0200

    cstat_only updated

--- cstat_onlyerror.py
+++ cstat_onlyerror.py
@@ -67,4 +67,6 @@ interp_method = arg_list[8]
 
 #Xset.save(model+"_error.xcm",info="a")
 
+if selection != "all": selection = np.int(selection)
+
 px.ml_get_errors(model+"_error",'cstat',selection = selection, blacklist = blacklist, n_cores=n_cores,level=input_level, plot_statistic = plot_statistic, interp_method = interp_method)
